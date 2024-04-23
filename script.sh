08705a6ce52160d3fa9bd37b1e852bbc4d750909
alihmohammad <alihmohammad@hotmail.com>
2024-04-23 21:28:43 +0000

    added more

--- script.sh
+++ script.sh
@@ -13,3 +13,31 @@ done
 echo "install? : $install"
 echo "gui? : $gui"
 echo "manjaro? : $manjaro"
+
+# Install packages
+if [ "$install" ]; then
+	if ["$manjaro"]; then
+		echo "Installing packages for Manjaro"
+	else
+		echo "Installing packages for Ubuntu"
+		sudo apt update
+		sudo apt install -y apt-transport-https curl gpg
+
+		# sudo ./install-repo.sh
+
+		sudo apt update
+		sudo apt upgrade -y
+
+		# Install apt packages
+		#sudo xargs apt install -y ./.extra/req.apt
+	fi
+
+
+	echo "Installation completed!"
+fi
+
+# Final setup
+cp ./.home /home/alihmohammad
+
+echo "Install Script complete!"
+
